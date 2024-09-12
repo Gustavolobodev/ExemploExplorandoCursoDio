4be06722e1647973ccb549e68bd263a43062145b
Gustavolobodev <gustavolobo.dev@gmail.com>
2024-09-12 20:05:58 -0300

    INTRODUÇÃO DICTIONARY

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,28 +1,119 @@
 ﻿using System.Globalization;
 using ExemploExplorando.Models;
 
+// INTRODUÇÃO DICTIONARY
 
-Queue<int> fila = new Queue<int>();
 
-fila.Enqueue(2);
-fila.Enqueue(4);
-fila.Enqueue(6);
-fila.Enqueue(8);
+Dictionary<string,string> estados = new Dictionary<string, string>();
 
-foreach(int item in fila)
-{   
-    Console.WriteLine(item);
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+Console.WriteLine(estados["Minas Gerais"]);
+
+// foreach(var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
+
+// Console.WriteLine("<------------------>");
+
+// estados.Remove("BA");
+// estados["SP"] = "São Paulo - valor alterado";
+
+// foreach(var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
+
+// VERIFICANDO SE A CHAVE JA EXISTE
+
+string chave = "BA2";
+
+if (estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: ");
+}
+else{
+    Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
 }
 
-Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
-fila.Enqueue(10);
 
-foreach(int item in fila)
-{   
-    Console.WriteLine(item);
-}
 
-Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+
+
+
+
+
+
+
+
+
+
+// EXPLORANDO STACK(PILHA)
+
+// Stack<int> pilha = new();
+
+// pilha.Push(4); // Insere um objeto no topo da pilha
+// pilha.Push(6);
+// pilha.Push(8);
+// pilha.Push(10);
+
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}"); // Remove e retorna o objeto para o topo da pilha
+
+// pilha.Push(20);
+
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EXPLORANDO QUEUE(FILA)
+
+// Queue<int> fila = new();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach(int item in fila)
+// {   
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+// foreach(int item in fila)
+// {   
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
 
 
 
--- ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f64228f7848349d6ac8abfc1a1bbe1d81c98d80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a476c590dd0098e79106045ecacf1eb9f873f9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
